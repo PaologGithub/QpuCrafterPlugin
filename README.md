8cc17c98f3f3f7e71ab6e82f371c618b1a2bd69c
PaologGithub <PaologGithub>
2025-06-15 12:53:19 +0200

    Base plugin configuration

---
+++ README.md
@@ -0,0 +1,2 @@
+# QpuCrafterPlugin
+The QpuCrafterPlugin is a base PaperMC 1.21 plugin, for the event "Question pour un Crafter".
---
+++ src/main/java/pao/paolog/plugins/paper/QpuCrafterPlugin.java
@@ -0,0 +1,15 @@
+package pao.paolog.plugins.paper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QpuCrafterPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
